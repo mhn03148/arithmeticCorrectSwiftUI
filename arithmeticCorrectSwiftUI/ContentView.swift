1d8e4fe067a1653dee59962e2b9c547b0952c60c
jujinhyeong <mhn03148@naver.com>
2023-06-20 13:42:50 +0900

    사칙연산 결과 OX 맞추기 게임 만들기 한화면에 임의의 곱셈 계산 식이 나옵니다 그 아래에는 O/X 버튼이 있습니다. 이 버튼을 누르면 화면 가장 아래(또는 가장 위)에 있는 다음의 내용에 반영됩니다.

--- arithmeticCorrectSwiftUI/ContentView.swift
+++ arithmeticCorrectSwiftUI/ContentView.swift
@@ -8,14 +8,127 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showMessage: String = ""
+    @State var symbalName: String = ""
+    @State var firstNumber: Int = Int.random(in: 0...10)
+    @State var secondNumber: Int = Int.random(in: 0...10)
+    @State var answerNumber: Int = 0
+    @State var presentRound: Int = 0
+    @State var flagNumber: Int = Int.random(in: 0...2)
+    @State var correctConunt: Int = 0
+    @State var incorrectCount: Int = 0
+    @State var startBool: Bool = false
+    
+    let totalRound: Int = 10
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Group{
+                Text("현재 라운드:\(presentRound) / 총라운드: \(totalRound)")
+                Spacer()
+                Spacer()
+            }.imageScale(.large).foregroundColor(.accentColor)
+            
+            if startBool == false {
+                Text("게임을 시작 하겠습니다.")
+                Button {
+                    startBool = true
+                    answerNumber = returnAnswerNumber(flagNumber, firstNumber, secondNumber)
+                }label: {
+                    Text("start!")
+                }
+            }else {
+                if presentRound < 11 {
+                    Text("\(firstNumber) X \(secondNumber) = \(answerNumber)")
+                }else {
+                    Text("게임이 끝났습니다!")
+                    
+                }
+            }
+            
+            Spacer()
+            HStack{
+                Text("맞춘 횟수: \(correctConunt)")
+                Text("틀린 횟수: \(incorrectCount)")
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                Button {
+                    if firstNumber * secondNumber == answerNumber && presentRound < 10{
+                    correctConunt += 1
+                    } else{
+                        incorrectCount += 1
+                    }
+                    if presentRound < 10 {
+                        presentRound += 1
+                        firstNumber = Int.random(in: 0...10)
+                        secondNumber = Int.random(in: 0...10)
+                        flagNumber = Int.random(in: 0...2)
+                        answerNumber = returnAnswerNumber(flagNumber, firstNumber, secondNumber)
+                    }
+                    
+                } label: {
+                    Image(systemName: "circle.square")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                }
+                
+                Spacer()
+                
+                Button{
+                    if firstNumber * secondNumber == answerNumber && presentRound < 10{
+                        incorrectCount += 1
+                    } else{
+                        correctConunt += 1
+                    }
+                    if presentRound < 10 {
+                        presentRound += 1
+                        firstNumber = Int.random(in: 0...10)
+                        secondNumber = Int.random(in: 0...10)
+                        flagNumber = Int.random(in: 0...2)
+                    }
+                    
+                } label: {
+                    Image(systemName: "multiply.square")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                }
+                Spacer()
+            }.font(.largeTitle)
+            Group{
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                Button {
+                    presentRound = 0
+                    startBool = false
+                    correctConunt = 0
+                    incorrectCount = 0
+                } label: {
+                    Text("reset")
+                }.font(.largeTitle)
+            }
+            Group{
+                Spacer()
+                Spacer()
+                Spacer()
+            }
         }
         .padding()
+    }
+    func returnAnswerNumber (_ plagNumber: Int, _ firstNum: Int, _ secondNum: Int)-> Int{
+    var returnNumber: Int = Int.random(in: 0...10)
+        switch (plagNumber){
+        case 0:
+            return returnNumber
+        case 1:
+            return returnNumber
+        case 2:
+            return firstNum * secondNum
+        default:
+            return 0
+        }
     }
 }
 
