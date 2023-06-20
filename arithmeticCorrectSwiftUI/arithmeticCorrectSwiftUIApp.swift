c59df8cdb3dbbf1fc69c9ebb834659055314f3f3
jujinhyeong <mhn03148@naver.com>
2023-06-20 10:40:50 +0900

    Initial Commit

---
+++ arithmeticCorrectSwiftUI/arithmeticCorrectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  arithmeticCorrectSwiftUIApp.swift
+//  arithmeticCorrectSwiftUI
+//
+//  Created by 주진형 on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct arithmeticCorrectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
